f93aa3a38d1c8aecf8a6cf2142440fdbbae1843c
leoncoutinho1 <leon.coutinho1@hotmail.com>
2020-07-26 13:29:13 -0300

    Ajustes layout

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -15,6 +15,21 @@ class PdfController extends Controller
         $data = consultCountry($country); 
         $charts = createlineChartsPdf($data['countryDataset']);
 
+        /* $pdf = PDF::loadView('layouts.pdf', [
+            'title' => 'Relatório',
+            'charts' => $charts,
+            'countryInfo' => $data['countryInfo'],
+            'currentConfirmed' => $data['currentConfirmed'],
+            'currentDeaths' => $data['currentDeaths'],
+            'dayOne' => $data['dayOne'],
+            'countries' => $data['countries'] 
+        ]);
+
+        $pdf->setOption('enable-javascript', true);
+        $pdf->setOption('javascript-delay', 5000);
+
+        return $pdf->download('teste.pdf'); */
+
         return view('layouts.pdf', [
             'title' => 'Relatório',
             'charts' => $charts,
